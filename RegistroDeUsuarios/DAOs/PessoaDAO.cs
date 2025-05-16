185f2575d0610b3f80618455cda027f32d7bdbc2
20240525 <20240525@LABMV01-02>
2025-05-16 20:46:07 -0300

    Adicionar arquivos de projeto.

---
+++ RegistroDeUsuarios/DAOs/PessoaDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using RegistroDeUsuarios.Infra.DataBase;
+using RegistroDeUsuarios.Models;
+
+namespace RegistroDeUsuarios.DAOs
+{
+    public class PessoaDAO
+    {
+        private ConnectDatabase dbConnection;
+
+        public PessoaDAO()
+        {
+            try { 
+                dbConnection = new ConnectDatabase();
+            } catch(Exception e) {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        public void Create(Pessoa pessoa)
+        {
+            string Sql = "INSERT INTO pessoas" + "(name, email, phone) VALUES (@name, @email, @telefone)";
+
+            try
+            {
+                var cmd = dbConnection.Connect().CreateCommand();
+            }
+            catch (Exception e) { }
+        }
+    }
+}
---
+++ RegistroDeUsuarios/Models/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroDeUsuarios.Models
+{
+    public class Pessoa
+    {
+        private int ID { get; set; }
+        private string Name { get; set; }
+        private string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
