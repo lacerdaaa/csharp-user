28c8351a6887dd3205ff0629009247809f5ab4cb
Eduardo Lacerda <137107996+lacerdaaa@users.noreply.github.com>
2025-05-19 19:51:16 -0300

    feat(): complete crud

--- RegistroDeUsuarios/DAOs/PessoaDAO.cs
+++ RegistroDeUsuarios/DAOs/PessoaDAO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
@@ -8,13 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using RegistroDeUsuarios.Infra.DataBase;
 using RegistroDeUsuarios.Models;
-
 namespace RegistroDeUsuarios.DAOs
 {
     public class PessoaDAO
     {
         private ConnectDatabase dbConnection;
-
         public PessoaDAO()
         {
             try { 
@@ -23,11 +21,9 @@ namespace RegistroDeUsuarios.DAOs
                 MessageBox.Show(e.Message);
             }
         }
-
         public void Create(Pessoa pessoa)
         {
             string Sql = "INSERT INTO pessoas" + "(name, email, phone) VALUES (@n, @e, @p)";
-
             try
             {
                 var cmd = dbConnection.Connect().CreateCommand();
@@ -35,7 +31,6 @@ namespace RegistroDeUsuarios.DAOs
                 cmd.Parameters.AddWithValue("@n", pessoa.Name);
                 cmd.Parameters.AddWithValue("@e", pessoa.Email);
                 cmd.Parameters.AddWithValue("@p", pessoa.Phone);
-
                 cmd.ExecuteNonQuery();
             }
             catch (Exception e)
@@ -46,12 +41,10 @@ namespace RegistroDeUsuarios.DAOs
                 dbConnection.Desconnect();
             }
         }
-
         public List<Pessoa> GetAll()
         {
             string sql = "SELECT * FROM PESSOAS";
             List<Pessoa> listaPessoa = new List<Pessoa>();
-
             try
             {
                 var cmd = dbConnection.Connect().CreateCommand();
@@ -64,18 +57,87 @@ namespace RegistroDeUsuarios.DAOs
                     pessoa.Name = result.GetString(1);
                     pessoa.Email = result.GetString(2);
                     pessoa.Phone = result.GetString(result.GetOrdinal("Phone"));
-
                     listaPessoa.Add(pessoa);
                 }
-                    result.Close();
+                result.Close();
             }
             catch (Exception e) {
                 MessageBox.Show(e.Message);
             }
-
+            finally {
+                dbConnection.Desconnect();
+            }
             return listaPessoa;
         }
-
-       
+        
+        public Pessoa GetById(int id)
+        {
+            string sql = "SELECT * FROM PESSOAS WHERE ID = @id";
+            Pessoa pessoa = null;
+            try
+            {
+                var cmd = dbConnection.Connect().CreateCommand();
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@id", id);
+                var result = cmd.ExecuteReader();
+                if (result.Read())
+                {
+                    pessoa = new Pessoa();
+                    pessoa.ID = result.GetInt32(0);
+                    pessoa.Name = result.GetString(1);
+                    pessoa.Email = result.GetString(2);
+                    pessoa.Phone = result.GetString(result.GetOrdinal("Phone"));
+                }
+                result.Close();
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.Message);
+            }
+            finally {
+                dbConnection.Desconnect();
+            }
+            return pessoa;
+        }
+        
+        public void Update(Pessoa pessoa)
+        {
+            string sql = "UPDATE PESSOAS SET name = @n, email = @e, phone = @p WHERE id = @id";
+            try
+            {
+                var cmd = dbConnection.Connect().CreateCommand();
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@n", pessoa.Name);
+                cmd.Parameters.AddWithValue("@e", pessoa.Email);
+                cmd.Parameters.AddWithValue("@p", pessoa.Phone);
+                cmd.Parameters.AddWithValue("@id", pessoa.ID);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally {
+                dbConnection.Desconnect();
+            }
+        }
+        
+        public void Delete(int id)
+        {
+            string sql = "DELETE FROM PESSOAS WHERE id = @id";
+            try
+            {
+                var cmd = dbConnection.Connect().CreateCommand();
+                cmd.CommandText = sql;
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally {
+                dbConnection.Desconnect();
+            }
+        }
     }
 }
